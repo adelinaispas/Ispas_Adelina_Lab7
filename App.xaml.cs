4799416acde16c1bade8b2cad2dbf43c607444ac
ADELINA-MARIA ISPAS <adelina.ispas@stud.ubbcluj.ro>
2023-11-23 17:43:08 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Ispas_Adelina_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ispas_Adelina_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
