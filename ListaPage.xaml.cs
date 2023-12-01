d3652d28c3b7947a750e947bc1ad28e739798e7f
ADELINA-MARIA ISPAS <adelina.ispas@stud.ubbcluj.ro>
2023-12-01 17:25:53 +0200

    Laborator8

---
+++ ListaPage.xaml.cs
@@ -0,0 +1,24 @@
+using Ispas_Adelina_Lab7.Models;
+namespace Ispas_Adelina_Lab7;
+
+public partial class ListaPage : ContentPage
+{
+	public ListaPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+}
---
+++ Models/ShopList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+namespace Ispas_Adelina_Lab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [SQLite.MaxLengthAttribute(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
